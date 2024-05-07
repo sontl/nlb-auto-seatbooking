529df1ff1dac4d12eb228bc228189e9b17622aa7
Son Tran Lam <sontl@mac.com>
2024-05-07 14:45:50 +0800

    update checkin with some logs

--- checkin.js
+++ checkin.js
@@ -5,7 +5,7 @@ const cron = require('node-cron');
 async function runPuppeteer() {
   try {
     // Launch the browser and open a new blank page
-    console.log('Running Puppeteer script...');
+    console.log(`Running NLB Automation Checkin at ...${printCurrentTime()}`);
     const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox'],
@@ -14,10 +14,9 @@ async function runPuppeteer() {
     const page = await browser.newPage();
 
     const context = browser.defaultBrowserContext();
-    await context.overridePermissions(
-      'https://www.nlb.gov.sg/seatbooking/mybookings',
-      ['geolocation']
-    );
+
+    const bookingUrl = 'https://www.nlb.gov.sg/seatbooking/mybookings';
+    await context.overridePermissions(bookingUrl, ['geolocation']);
 
     // Navigate the page to a URL
     await page.goto('https://signin.nlb.gov.sg/authenticate/login');
@@ -38,21 +37,48 @@ async function runPuppeteer() {
     await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // Go to My Booking page
-    await page.goto('https://www.nlb.gov.sg/seatbooking/mybookings');
+    await page.goto(bookingUrl);
 
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector('text/Today');
-    const fullTitle = await textSelector?.evaluate((el) => el.textContent);
     await page.setGeolocation({
       latitude: 1.3500680243819974,
       longitude: 103.87314643603517,
     });
 
-    console.log(fullTitle);
     // Wait for 5 seconds
-    await new Promise((resolve) => setTimeout(resolve, 5000));
-
     await new Promise((resolve) => setTimeout(resolve, 2000));
+    await page.evaluateOnNewDocument(() => {
+      navigator.geolocation.getCurrentPosition = (cb) => {
+        setTimeout(() => {
+          cb({
+            coords: {
+              accuracy: 21,
+              altitude: null,
+              altitudeAccuracy: null,
+              heading: null,
+              latitude: 1.3500680243819974,
+              longitude: 103.87314643603517,
+              speed: null,
+            },
+          });
+        }, 1000);
+      };
+    });
+    // if checkin is successful, there will be notification on the top with text Check-in is successful
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 15000));
+      const textSelector = await page.waitForSelector(
+        'text/Check-in is successful'
+      );
+      const status = await textSelector?.evaluate((el) => el.textContent);
+
+      if (status) {
+        console.log('Checkedin!');
+      } else {
+        console.log('Failed to checkin!');
+      }
+    } catch (error) {
+      console.log('Failed to checkin!');
+    }
     await browser.close();
   } catch (error) {
     console.error(error);
@@ -60,7 +86,21 @@ async function runPuppeteer() {
 }
 
 // Schedule the Puppeteer script to run every minute
-task = cron.schedule('0 */1 * * * *', runPuppeteer);
+task = cron.schedule('0 */30 * * * *', runPuppeteer);
+
+runPuppeteer();
 
 // To stop the cron task later
 // task.stop();
+function printCurrentTime() {
+  const now = new Date();
+  const day = String(now.getDate()).padStart(2, '0');
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+  const year = now.getFullYear();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+
+  const formattedTime = `${day}-${month}-${year} ${hours}:${minutes}`;
+  // console.log(formattedTime);
+  return formattedTime;
+}
